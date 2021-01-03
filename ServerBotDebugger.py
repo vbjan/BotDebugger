889cc816ff3ea6d0db6089ced96d3d01cb0f56c3
vbjan <jan-philipp@bassewitz>
2021-01-03 16:35:19 +0100

    second iteration

--- ServerBotDebugger.py
+++ ServerBotDebugger.py
@@ -3,10 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
 import time
 
-PATH = "/usr/bin/chromedriver"
-#PATH = "/Users/jan-philippvonbassewitz/bot_automation/chromedriver"
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+#PATH = "/usr/bin/chromedriver"
+PATH = "/Users/jan-philippvonbassewitz/bot_automation/chromedriver"
 driver = webdriver.Chrome(PATH)
 print("Bot is successfully initialized")
 
