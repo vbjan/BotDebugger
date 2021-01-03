acb92a9e1594312ef461175c62f542cfc46830aa
vbjan <jan-philipp@bassewitz>
2021-01-03 16:44:50 +0100

    third iteration

--- ServerBotDebugger.py
+++ ServerBotDebugger.py
@@ -11,9 +11,9 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-PATH = "/usr/bin/chromedriver"
-#PATH = "/Users/jan-philippvonbassewitz/bot_automation/chromedriver"
-driver = webdriver.Chrome(PATH)
+#PATH = "/usr/bin/chromedriver"
+PATH = "/Users/jan-philippvonbassewitz/bot_automation/chromedriver"
+driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
 print("Bot is successfully initialized")
 
 driver.get('https://www.asvz.ch/426-sportfahrplan?f[0]=facility:45598&f[1]=sport:122920')
